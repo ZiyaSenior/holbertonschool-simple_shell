a93eee0340a714da13e573d94ab0815d565874dc
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-22 16:59:59 +0400

    Create read_line.c

---
+++ read_line.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * read_line - Reads a line of input from stdin.
+ *
+ * Return: The input line.
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+
+	getline(&line, &bufsize, stdin);
+	return (line);
+}
