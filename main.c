5ac4719649c5045c2b1f04cf5aaa54c41e306f23
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-25 14:22:53 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,77 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the simple shell.
- * @argc: Argument count.
- * @argv: Argument vector.
+ * get_path_env - gets PATH value from environ
  *
- * Return: Always 0 (success).
+ * Return: PATH string or NULL
  */
-int main(int argc, char **argv)
+static char *get_path_env(void)
 {
-	(void)argc;
-	(void)argv;
+	int i;
 
-	shell_loop();
-	return (0);
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			return (environ[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * search_in_dirs - searches command in PATH directories
+ * @command: command name
+ * @path_env: PATH string
+ * @full_path: buffer to store result
+ *
+ * Return: full_path if found, NULL otherwise
+ */
+static char *search_in_dirs(char *command, char *path_env, char *full_path)
+{
+	char *path_copy;
+	char *dir;
+
+	path_copy = strdup(path_env);
+	if (path_copy == NULL)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		snprintf(full_path, 1024, "%s/%s", dir, command);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
+
+/**
+ * find_command - search command in PATH
+ * @command: command name
+ *
+ * Return: full path if found, otherwise NULL
+ */
+char *find_command(char *command)
+{
+	char *path_env;
+	static char full_path[1024];
+
+	if (strchr(command, '/'))
+	{
+		if (access(command, X_OK) == 0)
+			return (command);
+		return (NULL);
+	}
+
+	path_env = get_path_env();
+	if (path_env == NULL || path_env[0] == '\0')
+		return (NULL);
+
+	return (search_in_dirs(command, path_env, full_path));
 }
